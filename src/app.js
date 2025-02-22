fad518a9d57832f314ee37e1b79462bbcc35e2b9
sergiorjuel <110314668+sergiorjuel@users.noreply.github.com>
2025-02-22 01:17:46 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ['the', 'our', 'its', 'your', 'their'];
+let adj = ['great', 'big', 'short', 'little', 'amazing'];
+let noun = ['jogger', 'racoon', 'apple', 'mountain', 'guitar'];
+
+function generateDomains(pronoun, adj, noun) {
+  let domains = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        domains.push(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+  return domains;
+}
+
+
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(generateDomains(pronoun, adj, noun));
+  document.querySelector('#domains').innerHTML = generateDomains(pronoun, adj, noun);
 };
